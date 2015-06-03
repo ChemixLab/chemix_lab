944d8aab8a6db2e4b763f539cb79613939b4b75e
Safrana Shereen <Safrana Shereen@10.20.10.230>
2015-06-03 14:12:07 -0700

    chemixdb updated

--- DB/chemixdb.sql
+++ DB/chemixdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 03, 2015 at 07:44 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Host: localhost
+-- Generation Time: Jun 03, 2015 at 02:08 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -23,6 +22,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `chemical`
+--
+
+CREATE TABLE IF NOT EXISTS `chemical` (
+  `chemical_id` int(11) NOT NULL,
+  `chemical_name` text NOT NULL,
+  `chemical_notation` text NOT NULL,
+  `image` longblob NOT NULL,
+  PRIMARY KEY (`chemical_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chemical`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_reaction`
 --
 
@@ -33,6 +51,14 @@ CREATE TABLE IF NOT EXISTS `tb_reaction` (
   `observation` text NOT NULL,
   `cataliist` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_reaction`
+--
+
+INSERT INTO `tb_reaction` (`reaction_id`, `input_chemical`, `output_chemical`, `observation`, `cataliist`) VALUES
+(123, 'sfgfg', 'sfgfg', 'sf', 'gfg'),
+(12, 'sfbgfb', 'dbs', 'ffggf', 'dgg\r\n');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
