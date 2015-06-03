e801579c8c0a2da8e12cf82947d06b6245137968
fana <fana@192.168.5.48>
2015-06-03 14:50:45 -0700

    add chemicals

--- DB/observation.php
+++ DB/observation.php
@@ -8,16 +8,16 @@ $dbname = "chemixdb";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	die("Connection failed: " . $conn->connect_error);
+}
 
-$sql = "INSERT INTO chemical (chemical_name, chemical_notation,image)
-VALUES ('sodium choloride','Nacl','img1')";
+$sql = "INSERT INTO observation (observation_name, image)
+VALUES ('color change','img2')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+	echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+	echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
