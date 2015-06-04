b3841f5ac220251dea283a8449091faf5fa21265
nifrasismail <nifrasismail@gmail.com>
2015-06-04 23:29:22 +0530

    little commit

--- reactionCreatorModule/controller.js
+++ reactionCreatorModule/controller.js
@@ -24,7 +24,7 @@ listApp.controller('userCtrl', function($scope, $http) {
 	$scope.addOutputChemicals = function() {
 		$scope.outputChemicals.push($scope.newOutputChemical);
 	}
-	$scope.removeOutputItem = function(index) {
+	$scope.removeOutputChemical= function(index) {
 		$scope.outputChemicals.splice(index, 1);
 	}
 
@@ -38,32 +38,13 @@ listApp.controller('userCtrl', function($scope, $http) {
 
 	$scope.catalists = [];
 	$scope.addCatalists = function() {
-		$scope.catalists.push($scope.newObservation);
+		$scope.catalists.push($scope.newCatalist);
 	}
 	$scope.removeCatalists = function(index) {
 		$scope.catalists.splice(index, 1);
 	}
 
-	// Visibilities
-	$scope.hideMe1 = function() {
-		$scope.myVar1 = false;
-		$scope.myVar2 = true;
-	}
 
-	$scope.hideMe2 = function() {
-		$scope.myVar2 = false;
-		$scope.myVar3 = true;
-	}
-
-	$scope.hideMe3 = function() {
-		$scope.myVar3 = false;
-		$scope.myVar4 = true;
-	}
-
-	$scope.hideMe4 = function() {
-		$scope.myVar4 = false;
-		$scope.myVar5 = true;
-	}
 
 	/** function to add details for products in mysql referecing php * */
 
